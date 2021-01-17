5073e19f9043710f30a8e9077ab58c6434a21d16
KaushikIyer16 <kaushiiyer@gmail.com>
2021-01-17 16:44:41 +0530

    small typo fixes

--- src/commands/create-directory.js
+++ src/commands/create-directory.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 module.exports = function (projectName, srcDirectory) {
-  /* add validations and error when the directory already exists */
+  /* add validations and error when the directory already exists and standardize*/
   if (!fs.existsSync(srcDirectory + "/" + projectName)) {
     fs.mkdirSync(srcDirectory + "/" + projectName);
   }
